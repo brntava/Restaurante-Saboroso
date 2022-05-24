d0336f003806f4077569ab87bd97c9b1933e3264
brntava <bruno_tava7@hotmail.com>
2022-05-24 17:55:47 -0300

    exibindo dados do banco de dados com ejs embedded

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query(
+    `SELECT * FROM tb_menus ORDER BY title`,
+    (err, results) => {
+
+      if(err){
+        console.error(err)
+      }
+
+      res.render('index', { 
+        title: 'Restaurante Saboroso!' ,
+        menus: results
+      });
+
+    }
+  )
+
 });
+
+router.get('/contact', function(req, res, next) {
+
+  res.render('contact', {
+    title: 'Contato - Restaurante Saboroso'
+  })
+
+})
+
+router.get('/menu', function(req, res, next) {
+
+  res.render('menu', {
+    title: 'Menu - Restaurante Saboroso'
+  })
+
+})
+
+router.get('/reservation', function(req, res, next) {
+
+  res.render('reservation', {
+    title: 'Reservas - Restaurante Saboroso'
+  })
+
+})
+
+router.get('/services', function(req, res, next) {
+
+  res.render('services', {
+    title: 'Servicos - Restaurante Saboroso'
+  })
+
+})
 
 module.exports = router;
