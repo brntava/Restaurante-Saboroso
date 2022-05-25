67e8188ef13adf0e63615e8c62cff34385f88049
brntava <bruno_tava7@hotmail.com>
2022-05-25 13:48:50 -0300

    organizando o conteudo com include

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,43 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(
-    `SELECT * FROM tb_menus ORDER BY title`,
-    (err, results) => {
+  menus.getMenus().then(results => {
 
-      if(err){
-        console.error(err)
-      }
+    res.render('index', { 
+      title: 'Restaurante Saboroso!' ,
+      menus: results
+    });
 
-      res.render('index', { 
-        title: 'Restaurante Saboroso!' ,
-        menus: results
-      });
-
-    }
-  )
+  })
 
 });
 
 router.get('/contact', function(req, res, next) {
 
   res.render('contact', {
-    title: 'Contato - Restaurante Saboroso'
+    title: 'Contato - Restaurante Saboroso',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga ola!'
   })
 
 })
 
 router.get('/menu', function(req, res, next) {
 
-  res.render('menu', {
-    title: 'Menu - Restaurante Saboroso'
+  menus.getMenus().then(results => {
+
+    res.render('menu', {
+      title: 'Menu - Restaurante Saboroso',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    })
+
   })
 
 })
@@ -42,7 +45,9 @@ router.get('/menu', function(req, res, next) {
 router.get('/reservation', function(req, res, next) {
 
   res.render('reservation', {
-    title: 'Reservas - Restaurante Saboroso'
+    title: 'Reservas - Restaurante Saboroso',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma mesa!'
   })
 
 })
@@ -50,7 +55,9 @@ router.get('/reservation', function(req, res, next) {
 router.get('/services', function(req, res, next) {
 
   res.render('services', {
-    title: 'Servicos - Restaurante Saboroso'
+    title: 'Servicos - Restaurante Saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
   })
 
 })
