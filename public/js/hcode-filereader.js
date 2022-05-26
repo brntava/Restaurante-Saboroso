b931d8858d7229ea11eef3968efb751f5e253776
brntava <bruno_tava7@hotmail.com>
2022-05-26 17:03:07 -0300

    criado plugin pra ler e exibir a imagem do produto

---
+++ public/js/hcode-filereader.js
@@ -0,0 +1,50 @@
+class HcodeFileReader{
+
+    constructor(inputEl, imgEl){
+
+        this.inputEl = inputEl;
+        this.imgEl = imgEl;
+
+        this.initInputEvents();
+
+    }
+
+    initInputEvents(){
+
+        document.querySelector(this.inputEl).addEventListener('change', e =>{
+
+            this.reader(e.target.files[0]).then(result => {
+
+                document.querySelector(this.imgEl).src = result
+
+            })
+
+        });
+
+    }
+
+    reader(file){
+
+        return new Promise((resolve, reject) => {
+
+            let reader = new FileReader();
+
+            reader.onload = () => {
+    
+                resolve(reader.result)
+    
+            };
+
+            reader.onerror = () => {
+
+                reject('Nao foi possivel ler a imagem')
+
+            }
+    
+            reader.readAsDataURL(file);
+
+        })
+
+    }
+
+}
