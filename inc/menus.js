6b2ff6c3d209059f0d42f716e7e9baee9c01cf6c
brntava <bruno_tava7@hotmail.com>
2022-05-26 17:55:29 -0300

    exclusao e atualizacao dos dados

--- inc/menus.js
+++ inc/menus.js
@@ -26,20 +26,53 @@ module.exports = {
 
       return new Promise((resolve, reject) => {
 
-        fields.photo = `images/${path.parse(files.photo.path).base}`
+        fields.photo = `images/${path.parse(files.photo.path).base}`;
 
-        conn.query(`
-          INSERT INTO tb_menus (title, description, price, photo)
-          VALUES (?, ?, ?, ?)
-        `,[
+        let query, queryPhoto = '',params = [
             fields.title,
             fields.description,
             fields.price,
-            fields.photo
-        ], (err, results) =>{
+        ];
+
+        // Verificar se tem foto
+
+        if(files.photo.name){
+
+          queryPhoto = ',photo = ?';
+          params.push(fields.photo);
+
+        }
+
+        if(parseInt(fields.id) > 0){
+
+          params.push(fields.id);
+
+          query = `
+            UPDATE tb_menus SET title = ?, description = ?, price = ? ${queryPhoto}
+            WHERE id = ?
+          `;
+
+        } else{
+
+          if(!files.photo.name){
+
+            reject('Envie a foto do prato!');
+
+          }
+
+          query = `
+            INSERT INTO tb_menus (title, description, price, photo)
+            VALUES (?, ?, ?, ?)
+          `;
+
+        }
+
+        conn.query(query, params, (err, results) =>{
 
           if(err){
+
             reject(err)
+
           } else{
 
             resolve(results)
@@ -49,6 +82,32 @@ module.exports = {
         })
 
       });
+
+    },
+
+    delete(id){
+
+      return new Promise((resolve, reject) => {
+
+        conn.query(`
+          DELETE FROM tb_menus WHERE id = ?
+        `, [
+          id
+        ], (err, results) => {
+
+          if(err){
+            
+            reject(err)
+
+          } else{
+
+            resolve(results)
+
+          }
+
+        });
+
+      })
 
     }
 
